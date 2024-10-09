6a6dc1b7f68c8376bae604db3ecb4a03d47f90c1
alex-tiutiunnyk <tytynnik6377@gmail.com>
2024-10-09 23:00:02 +0300

    init commit (working version)

---
+++ app/src/main/java/com/example/test/screens/MapScreen.kt
@@ -0,0 +1,23 @@
+package com.example.test.screens
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.rememberCameraPositionState
+
+@Composable
+fun MapScreen() {
+    // Initialize the camera position state, which controls the camera's position on the map
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(LatLng(51.5074, -0.1278), 10f)
+    }
+
+    // Display the Google Map without
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState
+    )
+}
